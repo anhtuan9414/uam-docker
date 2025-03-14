fadb6685b878c1fdd22838edde86752e803813b5
TuanNA <30660319+anhtuan9414@users.noreply.github.com>
2025-03-14 13:04:06 +0700

    Update entrypoint.sh

--- uam-swarm/entrypoint.sh
+++ uam-swarm/entrypoint.sh
@@ -6,7 +6,8 @@ rm $file
 wget --no-check-certificate https://github.com/anhtuan9414/temp-2/raw/main/$file
 dpkg -i /tmp/$file
 cd /opt/uam/
-echo "[net]" >> /root/.uam/uam.ini
-container_ip="$(hostname -i)"
-echo "listens=[${container_ip}]:$2" >> /root/.uam/uam.ini
-./uam --pk $1 --http [0.0.0.0]:17099 --no-ui
+#echo "[net]" >> /root/.uam/uam.ini
+#container_ip="$(hostname -i)"
+#echo "listens=[${container_ip}]:$2" >> /root/.uam/uam.ini
+#./uam --pk $1 --http [0.0.0.0]:17099 --no-ui
+./uam --pk $1 --no-ui
